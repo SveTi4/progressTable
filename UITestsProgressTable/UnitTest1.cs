3268c096e4201a20470ce4c5ef77a6e9bab2550a
SveTi4 <studymail2.7.53@gmail.com>
2023-02-26 23:41:25 +0700

    Add student

---
+++ UITestsProgressTable/UnitTest1.cs
@@ -0,0 +1,28 @@
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.VisualTree;
+
+namespace UITestsProgressTable;
+
+public class UnitTest1
+{
+    [Fact]
+    public async void add_student()
+    {
+        var app = AvaloniaApp.GetApp();
+        var mainWindow = AvaloniaApp.GetMainWindow();
+
+        await Task.Delay(100);
+
+        var listBoxItems = mainWindow.GetVisualDescendants().OfType<ListBox>().First().GetVisualDescendants().OfType<ListBoxItem>();
+
+        var buttonAdd = mainWindow.GetVisualDescendants().OfType<Button>().First(b => b.Name == "AddStudentButton");
+
+        var textBox = mainWindow.GetVisualDescendants().OfType<TextBox>().First(t => t.Name == "newStudent");
+
+        textBox.Text = "Dima";
+        buttonAdd.Command.Execute(buttonAdd.CommandParameter);
+
+        Assert.True(listBoxItems.Count() == 2);
+    }
+}
